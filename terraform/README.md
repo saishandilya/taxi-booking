45c777a6847debec9a108a252b2c21406a1a672b
sai shandilya <bathula.shandilya@gmail.com>
2025-02-15 17:11:46 +0530

    feat: adding terraform modules to repo

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+# DevOps End to End Project Practice
+
+This is a repository to build AWS Infrastructure for DevOps Practice Project using Terraform.
---
+++ terraform/eks-infra-using-pipeline/output.tf
@@ -0,0 +1,7 @@
+output "eks_cluster_id" {
+    value = module.eks.cluster_id_value
+}
+
+output "eks_cluster_endpoint" {
+    value = module.eks.cluster_endpoint
+}
